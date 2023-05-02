ada470cf2b64423fc5732475062b39a95c5c0b35
dev-nakasho <shota9988@gmail.com>
2023-05-02 13:32:48 +0900

    add property for Person class

--- sample.php
+++ sample.php
@@ -3,10 +3,12 @@
 class Person {
   private $name;
   private $age;
+  private $height;
 
-  public function __construct($name, $age) {
+  public function __construct($name, $age, $height) {
     $this->name = $name;
     $this->age = $age;
+    $this->age = $height;
   }
 
   public function hello() {
